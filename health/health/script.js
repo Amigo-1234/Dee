1a2cdaf933235da654cf55813319df11a631ca5f
Amigo-1234 <olamidesamson841@gmail.com>
2025-03-07 00:10:13 +0000

    files upload

--- health/health/script.js
+++ health/health/script.js
@@ -1,34 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    function counter(id , start , end, duration){
-     let obj= document.getElementById(id),
-     current = start,
-     range = end - start,
-     increment = end > start ? 1 : -1 ,
-     step = Math.abs(Math.floor(duration / range)),
-     timer = setInterval(() => {
-         current += increment;
-         obj.textContent = current;
-         if( current == end){
-             clearInterval(timer);
-         }
-     }, step );
-    }
-    counter("count1", 0  , 2567 , 3000);
-    counter("count2", 100, 25, 2500);
-    counter("count3", 0  , 14,3000);
-    counter("count4", 0  , 4,3000);
- });
-   
+// Sliding Background Animation
+let index = 0;
+const slides = document.querySelector('.slides');
 
- let navbar = document.querySelector('.navbar');
- let menuIcon = document.querySelector('#menu-icon');
+function slideShow() {
+    index++;
+    if (index > 5) index = 0;
+    slides.style.transform = `translateX(-${index * 100}vw)`;
+}
+setInterval(slideShow, 2000); // Change every 2 seconds
 
- menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('fa-close');
-    navbar.classList.toggle('add');
- });
+// Responsive Navbar
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
 
- window.addEventListener('scroll', () =>{
-    navbar.classList.remove('add');
-    menuIcon.classList.remove('fa-close');
- });
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
