9bfa376fb37de25d38ab1be59e80e35eadfda653
Amigo-1234 <olamidesamson841@gmail.com>
2025-03-18 23:45:27 +0000

    files upload ..

--- health/health/script.js
+++ health/health/script.js
@@ -23,32 +23,3 @@ document.addEventListener("DOMContentLoaded", () => {
     counter("count4", 0, 4, 3000);
 });
 
-// Navbar Toggle
-let navbar = document.querySelector('.navbar');
-let menuIcon = document.querySelector('#menu-icon');
-
-if (menuIcon && navbar) {
-    menuIcon.addEventListener('click', () => {
-        menuIcon.classList.toggle('fa-close');
-        navbar.classList.toggle('add');
-    });
-
-    window.addEventListener('scroll', () => {
-        navbar.classList.remove('add');
-        menuIcon.classList.remove('fa-close');
-    });
-}
-
-const slider = document.getElementById('slider');
-const slides = document.querySelectorAll('.home-slide');
-let currentIndex = 0;
-
-// Slide every 4 seconds, stop at the last image
-const slideInterval = setInterval(() => {
-    currentIndex++;
-    if (currentIndex < slides.length) {
-        slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-    } else {
-        clearInterval(slideInterval); // Stop sliding at the last image
-    }
-}, 4000); // 4 seconds per slide
